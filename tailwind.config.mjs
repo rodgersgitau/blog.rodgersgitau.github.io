01f1ff1a1047b36c59d6d1752b836815fe7d78fb
Rodgers Gitau <rodgersgitau@gmail.com>
2023-10-29 06:34:27 +0300

    style: theming
    - add css for dark & light theme
    - add `Select` component
    - add `Icon` component
    - add `ThemeToggler` Preact component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "rgb(var(--text))",
+        background: "rgb(var(--background))",
+        primary: "rgb(var(--primary))",
+        secondary: "rgb(var(--secondary))",
+        accent: "rgb(var(--accent))",
+      },
+    },
+  },
+  plugins: [],
+};
